0f487b3cf5255a0283b27f25137fcb8bf99cd909
guilhermesantos <guilherme.milan9@gmail.com>
2019-05-20 08:34:48 -0300

    gramatica, frases

---
+++ horoscopo.pl
@@ -0,0 +1,19 @@
+/*artigo --> [o], [a].
+subst --> [Expansão],[diferença], [tema].
+v --> [é], [fica], [fará].
+prep --> [de].
+adv --> [hoje].
+pnt --> [.].
+*/
+s-->vp, pnt.
+vp-->n, np.
+vp-->v, vp.
+np--> art, nn.
+nn--> n | n, pp.
+n --> [expansão] | [tema].
+pp-->p, adv.
+v-->[é].
+art-->[o].
+p-->[de].
+adv-->[hoje].
+pnt-->[.].
